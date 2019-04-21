cc53402535d46f61990aa7f2b0e17ac173a8de06
Lucille Blumire <llblumire@gmail.com>
2019-04-21 13:48:42 +0100

    Switches underlying datastore to mysql

--- soclocker-server/src/database.rs
+++ soclocker-server/src/database.rs
@@ -2,10 +2,10 @@
 
 use rocket_contrib::database;
 
-use diesel::SqliteConnection;
+use diesel::MysqlConnection;
 
 /// Constructs the Database Connection from the supplied configuration. During
 /// development this is an SqliteConnection, however it will be swapped for a
 /// MySqlConnection closer to deployment.
 #[database("core_db")]
-pub struct CoreDbConn(SqliteConnection);
+pub struct CoreDbConn(MysqlConnection);
